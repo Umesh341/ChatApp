366e402aab057c9c41f4cee8b45135f5e65c5fb6
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-10-12 08:36:18 +0545

    finaly fixed biuld command

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,6 @@
 import {create} from "zustand";
 import {axiosInstance} from "../lib/axios";
 import toast from "react-hot-toast";
-import { get } from "mongoose";
 import { data } from "react-router-dom";
 import {useAuthStore} from "./useAuthStore"
 
